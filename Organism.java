2970f13c8290120c64a3f470873c33c45bed5dc4
VojtechMraz <162875288+VojtechMraz@users.noreply.github.com>
2024-06-08 14:50:03 +0200

    Update Organism.java

--- Organism.java
+++ Organism.java
@@ -1,13 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Vojta
- */
+
 public class Organism implements Movable{
     private int x;
     private int y;
